8965a80066cec0890c457884eb0e3196bea0307c
Milos Danilovic <danilovicmilos25@gmail.com>
2025-04-05 14:07:56 +0200

    Bugfix(Geenral):Fix responsive

--- src/Components/ProgressBar/ProgressBar.tsx
+++ src/Components/ProgressBar/ProgressBar.tsx
@@ -4,7 +4,8 @@ const ProgressBar = () => {
 
 
     return (
-        <ul className="steps w-full steps-vertical sm:steps-horizontal ">
+        <div className="w-full flex justify-center align-center">
+        <ul className="steps md:w-full gap-4 ">
             <li className="step step-primary cursor-pointer">
                 <span className="step-icon"> <MapPin size={20}/></span>
                 Postcode
@@ -38,6 +39,7 @@ const ProgressBar = () => {
 
             </li>
         </ul>
+        </div>
     );
 };
 
